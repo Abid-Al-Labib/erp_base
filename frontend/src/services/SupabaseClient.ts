bed98ebb34418f987f33f070721a2b2e2116f5a2
Abid-Al-Labib <getabidallabib@gmail.com>
2024-11-15 01:10:41 -0500

    attempting env variable

--- frontend/src/services/SupabaseClient.ts
+++ frontend/src/services/SupabaseClient.ts
@@ -1,7 +1,7 @@
 
 import { createClient } from '@supabase/supabase-js'
 
-const supabase_url="https://outgycradjrfbjglnyxr.supabase.co"
-const supabase_anon_key ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im91dGd5Y3JhZGpyZmJqZ2xueXhyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjExODcyNDIsImV4cCI6MjAzNjc2MzI0Mn0.vLBW-adZlymfFtfONdJlmgljgGwgck3EPIhSlphPNmA"
+const supabase_url= import.meta.env.VITE_SUPABASE_URL;
+const supabase_anon_key = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase_client = createClient(supabase_url, supabase_anon_key)
